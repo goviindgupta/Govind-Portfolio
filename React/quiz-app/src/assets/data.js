2b1a5bee55857a32ed79618862fc4fc117b5a0cf
Govind Kumar Gupta <govindkumargupta082@gmail.com>
2025-09-24 14:44:04 +0530

    first commit on React

---
+++ React/quiz-app/src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+      question: "Which device is required for the Internet connection?",
+      option1: "Modem",
+      option2: "Router",
+      option3: "LAN Cable",
+      option4: "Pen Drive",
+      ans: 1,
+    },
+    {
+      question: "Which continent has the highest number of countries?",
+      option1: "Asia",
+      option2: "Europe",
+      option3: "North America",
+      option4: "Africa",
+      ans: 4,
+    },
+    {
+      question: "Junk e-mail is also called?",
+      option1: "Spam",
+      option2: "Trash",
+      option3: "Spoof",
+      option4: "Sniffer script",
+      ans: 1,
+    },
+    {
+      question: "Which is the smallest continent in the world?",
+      option1: "Asia",
+      option2: "Australia",
+      option3: "Arctic",
+      option4: "Africa",
+      ans: 2,
+    },
+    {
+      question: "Which country is known as the Land of Rising Sun?",
+      option1: "Japan",
+      option2: "Korea",
+      option3: "China",
+      option4: "Thailand",
+      ans: 1,
+    },
+  ];
+  
